39c20eead113c3cbac3262b5bbfc80e80ae9e5b3
Alex Kovacs <alexkovacs@rocketmail.com>
2023-05-07 22:00:17 +0300

    Add support for sse

---
+++ chat-read/src/main/java/com/cpd/chatread/ReadController.java
@@ -0,0 +1,26 @@
+package com.cpd.chatread;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+@RestController
+@RequestMapping("/read")
+public class ReadController {
+
+    @GetMapping
+    public String test() {
+        return "Hello world";
+    }
+
+    @GetMapping(path = "/stream-flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamFlux() {
+        return Flux.interval(Duration.ofSeconds(65))
+                .map(sequence -> "Flux - " + LocalTime.now());
+    }
+}
---
+++ chat-write/src/main/java/com/cpd/chatwrite/WriteController.java
@@ -0,0 +1,9 @@
+package com.cpd.chatwrite;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/write")
+public class WriteController {
+}
