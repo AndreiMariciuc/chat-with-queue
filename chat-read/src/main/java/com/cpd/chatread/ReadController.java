bc825f7850a20a09f335a55664aca4f3b57077c3
Alex Kovacs <alexkovacs@rocketmail.com>
2023-05-10 15:39:10 +0300

    Remove test scheduled send

--- chat-read/src/main/java/com/cpd/chatread/ReadController.java
+++ chat-read/src/main/java/com/cpd/chatread/ReadController.java
@@ -4,31 +4,23 @@ import com.cpd.chatread.services.SubscribeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Sinks;
 
 @RestController
 @EnableScheduling
 @RequiredArgsConstructor
 @RequestMapping("/read")
 public class ReadController {
-    private final Sinks.Many<String> sink = Sinks.many().unicast().onBackpressureBuffer();
 
     private final SubscribeService subscribeService;
 
     @GetMapping
     public String test() {
         return "Hello world";
-    }
-
-    @GetMapping(path = "/test", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<String> streamFlux() {
-        return sink.asFlux();
     }
 
     @GetMapping(path = "/subscribe/{username}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
@@ -45,11 +37,5 @@ public class ReadController {
     public Flux<String> joinRoom(@PathVariable String roomName) {
         var sink = subscribeService.getOrCreateRoom(roomName);
         return sink.asFlux();
-    }
-
-    @Scheduled(fixedRate = 1000)
-    void run() {
-        System.out.println("Am trimis");
-        sink.emitNext("mai trimit ceva", Sinks.EmitFailureHandler.FAIL_FAST);
     }
 }
